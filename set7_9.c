79b233215bdb0354a8328833c29b02ad18012360
thalaraj <47813910+thalaraj@users.noreply.github.com>
2019-03-03 00:06:56 -0800

    Create set7_9.c

---
+++ set7_9.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int N,M,x;
+    scanf("%d %d",&N,&M);
+    x=N-M;
+    
+    if(x%2==0)
+    {
+    
+    printf("even");
+    }
+    else
+    {
+    
+        printf("odd");
+    }
+return 0;
+}
